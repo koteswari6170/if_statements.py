4bfa8d3f6a4bf839070e5aa7ab8902f0c4991a93
koteswari6170 <102402089+koteswari6170@users.noreply.github.com>
2022-04-19 14:25:39 +0530

    Add files via upload

---
+++ if statement.py
@@ -0,0 +1,32 @@
+# a=int(input("Enter the value of a:"))
+# b=int(input("Enter the value of b:"))
+# a,b=[int(x) for x in input().split()]
+# print(a,b)
+
+# a=int(input("Enter the value of a:"))
+# if(a==5):
+#     print("Yes equal")
+
+# a,b=[int(x) for x in input().split()]
+# if(a>b):
+#     print(f"{a}>{b} is {a>b}")
+# else:
+#     print(f"{a}<{b} is {a<b}")
+
+# a,b=[int(x) for x in input().split()]         #3 5
+# if(a>b):print("a is greater than b")
+# if(a<b):print("a is less than b")
+# if(a<=b):print("a is less than or equal to  b")
+# if(a>=b):print("a is greater than or equal to b")
+# if(a==b):print("a is equal to b")
+# if(a!=b):print("a is not equal to b")           #line number 18,19,22 printed
+
+# a,b=[int(x) for x in input().split()]         #3 5
+# if(a>b):print("a is greater than b")
+# elif(a<b):print("a is less than b")
+# elif(a<=b):print("a is less than or equal to  b")
+# elif(a>=b):print("a is greater than or equal to b")
+# elif(a==b):print("a is equal to b")
+# elif(a!=b):print("a is not equal to b")  #line number 26 printed will not go to the next line
+
+
